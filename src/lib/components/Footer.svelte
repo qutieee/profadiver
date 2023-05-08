68d245d3c1f9dd81bcee48beefaeedf93d229ea9
KevinXutie <kevincorpin40@gmail.com>
2023-05-08 10:37:17 +0800

    new first commit - novs

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,10 @@
+<footer class="footer footer-center mt-5 p-10 bottom-0 bg-base-100 text-white-200">
+  <div>
+    <img width="250" alt="logo" height="150" src="profadiver-logo.png" />
+    <p class="font-bold pt-8">
+      <br/> 
+    </p> 
+    <p>Copyright © 2023 - All right reserved</p>
+  </div> 
+
+</footer>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css'
+  	import Navbar from '$lib/components/Navbar.svelte'
+  	import Footer from '$lib/components/Footer.svelte'
+
+</script>
+
+	<Navbar />
+
+	<slot />
+
+	<Footer />
