1e3fbc5e576f41ad8d8f6bed3a61b996c3ffafad
tsifira17 <100056163+tsifira17@users.noreply.github.com>
2023-05-11 22:58:37 +0800

    promt updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,7 @@ async function saveName() {
 	
 </script>
 
-<div class="isolate bg-base-100 py-24">
+<div class="isolate bg-base-100 px-6 py-24 sm:py-32 lg:px-8">
 	<div
 		class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
 		aria-hidden="true"
@@ -25,8 +25,7 @@ async function saveName() {
 			style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
 		/>
 	</div>
-	
-	<div class="mx-auto max-w-2xl mt-20 text-center min-[320px]:text-center min-[320px]:mx-auto">
+	<div class="mx-auto max-w-2xl text-center">
 		<div class="m-8">
 			<h1 class="text-5xl font-bold">Welcome to</h1>
 			<img src="profadiver-logo.png" alt="logo"/>
