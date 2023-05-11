c5c3413ac7f44ebdf529665fd396558a7f1ea8c7
tsifira17 <100056163+tsifira17@users.noreply.github.com>
2023-05-12 03:53:01 +0800

    about updated

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -1,81 +1,89 @@
-
-
+<div class="bg-base-100">
 	<div class="relative isolate px-6 pt-1 lg:px-8">
-		<div class="isolate bg-base-100 px-6 py-24 sm:py-32 lg:px-8">
-			<div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]" aria-hidden="true">
-				<div class="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
+	  <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+			<div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
 			</div>
-			<div class="mx-auto max-w-2xl text-center">
-				<div class=" min-h-screen">
-					<div class="hero-content flex-col lg:flex-row-reverse">
-						<img src="about-us-img.png" class="max-w-sm rounded-lg " alt="f1"/>
-						<div>
-						<h1 class="text-5xl font-bold mb-6">About Us</h1>
-						<p class="text-1xl font-semi">Let's start a conversation and discover where it can take you with Profadiver, a web-based system providing mental healthcare companionship for those in need. By establishing a platform for individuals seeking help with their personal problems, Profadiver aims to assist those in need of someone to talk to and gradually normalize seeking consultation and support in the country.</p>
-						</div>
-					</div>
-
-					<div class="hero min-h-screen bg-base-100">
-						<div class="hero-content flex-col lg:flex-row">
-							<img src="img1.jpg" class="max-w-sm rounded-lg " alt="f2" />
-							<div class=" md:m-20">
-							<h1 class="text-5xl font-bold mb-6">Our Story</h1>
-							<p>Profadiver is developed by Wonder Pets, a group of college students from the University of Caloocan City (UCC) who are sleep-deprived but highly motivated to make it to the end of the semester. We also strive to make consulting and opening up about personal problems a common practice for everyone in need of someone to talk to about their problems.</p>
-						</div>
-					</div>
-					</div>
-
-					<div class="bg-base-100 py-24 sm:py-32">
-						<div class="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
-							<div class="max-w-2xl">
-								<h2 class="text-3xl font-bold tracking-tight mb-3 sm:text-4xl">Meet our team</h2>
-								<p>From BSCS 4-C</p>
-							</div>
-							<ul role="" class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
-								<li>
-									<div class="flex items-center gap-x-6">
-										<img class="h-16 w-16 rounded-full" src="/novs.jpg" alt="novie" />
-										<div>
-											<h3 class="text-base font-semibold leading-7 tracking-tight text-white">Novie Dame</h3>
-											<p class="text-sm font-semibold leading-6 text-indigo-600">Programmer</p>
-										</div>
-									</div>
-								</li>
-								<li>
-									<div class="flex items-center gap-x-6">
-										<img class="h-16 w-16 rounded-full" src="/rei.jpg" alt="rei" />
-										<div>
-											<h3 class="text-base font-semibold leading-7 tracking-tight text-white">Reilly Kyle</h3>
-											<p class="text-sm font-semibold leading-6 text-indigo-600">Researcher</p>
-										</div>
-									</div>
-								</li>
-								<li>
-									<div class="flex items-center gap-x-6">
-										<img class="h-16 w-16 rounded-full" src="/rose.jpg" alt="rose" />
-										<div>
-											<h3 class="text-base font-semibold leading-7 tracking-tight text-white">Maryrose</h3>
-											<p class="text-sm font-semibold leading-6 text-indigo-600">Tool</p>
-										</div>
-									</div>
-								</li>
-								<li>
-									<div class="flex items-center gap-x-6">
-										<img class="h-16 w-16 rounded-full" src="/su.jpg" alt="" />
-										<div>
-											<h3 class="text-base font-semibold leading-7 tracking-tight text-white">
-												Dominic Jhon
-											</h3>
-											<p class="text-sm font-semibold leading-6 text-indigo-600">Assistant Researcher</p>
-										</div>
-									</div>
-								</li>
-								<!-- More people... -->
-							</ul>
-						</div>
-					</div>
+	  </div>
+	  
+		<div class="mx-auto max-w-2xl py-32 ">
+			<div class="hero-content flex-col lg:flex-row">	
+				<div>
+					<h1 class="text-4xl font-bold tracking-tight text-white-100 sm:text-6xl">About Us</h1>
+					<p class="mt-6 leading-8">Let's start a conversation and discover where it can take you with Profadiver, a web-based system providing mental healthcare companionship for those in need. By establishing a platform for individuals seeking help with their personal problems, Profadiver aims to assist those in need of someone to talk to and gradually normalize seeking consultation and support in the country.</p>
+				</div>
+				<div>
+					<img src="profadiver-ali.png" class="max-w-sm mt-5" alt="f2" width="150" />
+					<img src="profadiver-logo.png" class="max-w-sm mt-5" alt="f2" width="150" />
 				</div>
 			</div>
+		</div>
 
+	  <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+		<div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+	  </div>
+
+	</div>
+
+	<div class=" min-h-screen">
+		<div class="hero min-h-screen bg-base-100">
+			<div class="hero-content flex-col lg:flex-row">
+				<img src="img1.jpg" class="max-w-sm rounded-lg " alt="f2" />
+				<div class=" md:m-20">
+					<h1 class="text-5xl font-bold mb-6">Our Story</h1>
+					<p>Profadiver is developed by Wonder Pets, a group of college students from the University of Caloocan City (UCC) who are sleep-deprived but highly motivated to make it to the end of the semester. We also strive to make consulting and opening up about personal problems a common practice for everyone in need of someone to talk to about their problems.</p>
+				</div>
+			</div>
+		</div>	
+	</div>
+
+	<div class=" py-20 sm:py-32">
+		<div class="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+			<div class="max-w-2xl">
+				<h2 class="text-3xl font-bold tracking-tight mb-3 sm:text-4xl">Meet our team</h2>
+				<p>From BSCS 4-C</p>
+			</div>
+			<ul role="" class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+				<li>
+					<div class="flex items-center gap-x-6">
+						<img class="h-16 w-16 rounded-full" src="/novs.jpg" alt="novie" />
+						<div>
+							<h3 class="text-base font-semibold leading-7 tracking-tight text-white">Novie Dame</h3>
+							<p class="text-sm font-semibold leading-6 text-indigo-600">Programmer</p>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="flex items-center gap-x-6">
+						<img class="h-16 w-16 rounded-full" src="/rei.jpg" alt="rei" />
+						<div>
+							<h3 class="text-base font-semibold leading-7 tracking-tight text-white">Reilly Kyle</h3>
+							<p class="text-sm font-semibold leading-6 text-indigo-600">Researcher</p>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="flex items-center gap-x-6">
+						<img class="h-16 w-16 rounded-full" src="/rose.jpg" alt="rose" />
+						<div>
+							<h3 class="text-base font-semibold leading-7 tracking-tight text-white">Maryrose</h3>
+							<p class="text-sm font-semibold leading-6 text-indigo-600">Tool</p>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="flex items-center gap-x-6">
+						<img class="h-16 w-16 rounded-full" src="/su.jpg" alt="" />
+						<div>
+							<h3 class="text-base font-semibold leading-7 tracking-tight text-white">
+								Dominic Jhon
+							</h3>
+							<p class="text-sm font-semibold leading-6 text-indigo-600">Assistant Researcher</p>
+						</div>
+					</div>
+				</li>
+				<!-- More people... -->
+			</ul>
 		</div>
 	</div>
+
+</div>
