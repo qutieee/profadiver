a91ed8a9e4488a0c97980ca096365fb9af34cf02
tsifira17 <100056163+tsifira17@users.noreply.github.com>
2023-05-11 18:33:55 +0800

    vercel

--- .vercel/output/functions/fn-0.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn-0.func/.svelte-kit/output/server/nodes/1.js
@@ -2,7 +2,7 @@
 
 export const index = 1;
 export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/entry/error.svelte.84ce8f61.js';
-export const imports = ["_app/immutable/entry/error.svelte.84ce8f61.js","_app/immutable/chunks/index.b52b6bba.js","_app/immutable/chunks/singletons.39efcc98.js"];
+export const file = '_app/immutable/entry/error.svelte.f6aeb1d4.js';
+export const imports = ["_app/immutable/entry/error.svelte.f6aeb1d4.js","_app/immutable/chunks/index.b52b6bba.js","_app/immutable/chunks/singletons.3c7e4aac.js"];
 export const stylesheets = [];
 export const fonts = [];
--- .vercel/output/functions/fn-0.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn-0.func/.svelte-kit/vercel-tmp/manifest.js
@@ -4,7 +4,7 @@ export const manifest = {
 	assets: new Set(["about-us-img.png","favicon.png","features1.png","features2.png","img1.jpg","novs.jpg","profadiver-ali.png","profadiver-logo.png","rei.jpg","rose.jpg","su.jpg"]),
 	mimeTypes: {".png":"image/png",".jpg":"image/jpeg"},
 	_: {
-		client: {"start":{"file":"_app/immutable/entry/start.4dc6a7c7.js","imports":["_app/immutable/entry/start.4dc6a7c7.js","_app/immutable/chunks/index.b52b6bba.js","_app/immutable/chunks/singletons.39efcc98.js"],"stylesheets":[],"fonts":[]},"app":{"file":"_app/immutable/entry/app.3d302aa0.js","imports":["_app/immutable/entry/app.3d302aa0.js","_app/immutable/chunks/preload-helper.41c905a7.js","_app/immutable/chunks/index.b52b6bba.js"],"stylesheets":[],"fonts":[]}},
+		client: {"start":{"file":"_app/immutable/entry/start.ac877093.js","imports":["_app/immutable/entry/start.ac877093.js","_app/immutable/chunks/index.b52b6bba.js","_app/immutable/chunks/singletons.3c7e4aac.js"],"stylesheets":[],"fonts":[]},"app":{"file":"_app/immutable/entry/app.289fab72.js","imports":["_app/immutable/entry/app.289fab72.js","_app/immutable/chunks/preload-helper.41c905a7.js","_app/immutable/chunks/index.b52b6bba.js"],"stylesheets":[],"fonts":[]}},
 		nodes: [
 			() => import('../output/server/nodes/0.js'),
 			() => import('../output/server/nodes/1.js'),
